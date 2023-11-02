3a7954d6b9c1be5d6e149c50ec68b394cf310310
selimcan sağdıç <selimcanscs1907@gmail.com>
2023-11-02 20:23:31 +0300

    components of not-found created

--- app/movie/[id]/page.js
+++ app/movie/[id]/page.js
@@ -1,10 +1,15 @@
 import MovieContainer from "@/containers/movie";
 import Movies from "@/mocks/movies.json";
+import { notFound } from "next/navigation";
 
 const MoviePage = ({ params }) => {
   const movieDetail = Movies.results.find((movie) => movie.id.toString() == params.id);
   if (!movieDetail) {
-    return <div>dasdasds</div>;
+    // return <div></div>;
+  };
+
+  if (!movieDetail){
+    notFound()
   }
 
   return <MovieContainer movieDetail={movieDetail} />;
