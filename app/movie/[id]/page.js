095ee3b73affa5369d0fd7510d1ee33c54368f84
selimcan sağdıç <selimcanscs1907@gmail.com>
2023-11-02 21:49:07 +0300

    updated

--- app/movie/[id]/page.js
+++ app/movie/[id]/page.js
@@ -1,15 +1,17 @@
+import React from "react";
 import MovieContainer from "@/containers/movie";
 import Movies from "@/mocks/movies.json";
 import { notFound } from "next/navigation";
 
-const MoviePage = ({ params }) => {
+const MoviePage = ({ params, searchParams }) => {
   const movieDetail = Movies.results.find((movie) => movie.id.toString() == params.id);
-  if (!movieDetail) {
-    // return <div></div>;
-  };
 
-  if (!movieDetail){
-    notFound()
+  if (!movieDetail) {
+    notFound();
+  }
+
+  if (searchParams.error === "true") {
+    throw new Error("Error");
   }
 
   return <MovieContainer movieDetail={movieDetail} />;
